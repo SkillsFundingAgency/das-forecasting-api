2dd36d5965806dbaf33d36168d69099f5230e9bd
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-02-12 08:44:26 +0000

    Initial project commit
    Initial project commit with Azure Table storage configuration provider

---
+++ src/SFA.DAS.Forecasting.Domain/Configuration/AzureActiveDirectoryConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Forecasting.Domain.Configuration
+{
+    public class AzureActiveDirectoryConfiguration
+    {
+        public string Tenant { get; set; }
+        public string Identifier { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Forecasting.Infrastructure/Configuration/StorageConfigParser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using SFA.DAS.Forecasting.Domain.Configuration;
+
+namespace SFA.DAS.Forecasting.Infrastructure.Configuration
+{
+    public class StorageConfigParser
+    {
+        public Dictionary<string, string> ParseConfig(ConfigurationItem configItem, string defaultSectionName)
+        {
+            var configDictionary = new Dictionary<string, string>();
+
+            var jsonObject = JObject.Parse(configItem.Data);
+
+            foreach (var child in jsonObject.Children())
+            {
+                foreach (var jToken in child.Children().Children())
+                {
+                    var child1 = (JProperty)jToken;
+                    configDictionary.Add($"{child.Path}:{child1.Name}", child1.Value.ToString());
+                }
+
+                if (string.IsNullOrEmpty(defaultSectionName))
+                {
+                    continue;
+                }
+
+                foreach (var jToken in child.Children())
+                {
+                    if (!jToken.Children().Any())
+                    {
+                        configDictionary.Add($"{defaultSectionName}:{jToken.Path}", jToken.Value<string>());
+                    }
+                }
+
+            }
+            return configDictionary;
+        }
+    }
+}
