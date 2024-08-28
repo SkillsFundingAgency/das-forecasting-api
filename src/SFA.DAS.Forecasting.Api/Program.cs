09a8c9590b0aeb5dacee529be428740905b1ba91
Jon Harris <jon.harris@digital.education.gov.uk>
2024-08-28 10:35:15 +0100

    refactor to make CreateWebHostBuilder private

--- src/SFA.DAS.Forecasting.Api/Program.cs
+++ src/SFA.DAS.Forecasting.Api/Program.cs
@@ -11,7 +11,7 @@ public class Program
         CreateWebHostBuilder(args).Build().Run();
     }
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
             .UseStartup<Startup>()
             .UseNLog();
